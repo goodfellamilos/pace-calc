9d8a1d7c37dacadd39c7fe85ed8b112b4704d43e
Milos Nikolic <mnikolic987@gmail.com>
2020-04-10 18:27:06 +0200

    Add constants

---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+export default {
+  gray: "#b3b3b3",
+  moderatePink: "#d44c7c",
+  mostlyBlack: "#1c1c1c",
+  veryDarkGray: "#282828",
+  veryLightGray: "#d9d9d9"
+};
---
+++ constants/units.js
@@ -0,0 +1,10 @@
+export const KM_IN_MILES = 0.62137119;
+export const MILE_IN_KM = 1.609344;
+
+export const MINUTE_IN_SECONDS = 60;
+export const HOUR_IN_SECONDS = 60 * MINUTE_IN_SECONDS;
+
+export const MIN_PER_KM_IN_KPH = 60;
+export const MIN_PER_KM_IN_MPH = 37.28227153424;
+export const MIN_PER_MILE_IN_KPH = 96.56064;
+export const MIN_PER_MILE_IN_MPH = 60;
