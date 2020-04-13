55a8bb94828bd70b17ffb6ce4d9b881aa2d77c60
Milos Nikolic <mnikolic987@gmail.com>
2020-04-13 15:13:52 +0200

    Add StyledTextInput component

---
+++ components/inputs/StyledTextInput.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, TextInput } from "react-native";
+
+import colors from "../../constants/colors";
+
+const StyledTextInput = props => (
+  <TextInput {...props} style={[styles.textInput, props.style]} />
+);
+
+export default StyledTextInput;
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderColor: colors.veryLightGray,
+    borderRadius: 4,
+    borderWidth: 1,
+    color: colors.veryLightGray,
+    fontFamily: "space-mono",
+    padding: 10,
+    textAlign: "center",
+    width: 50
+  }
+});
