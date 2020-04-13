d2ade174fd88f51a6bbe89f8e59c99ff3dbe8107
Milos Nikolic <mnikolic987@gmail.com>
2020-04-13 15:13:15 +0200

    Add StyledText component

---
+++ components/texts/StyledText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+import colors from "../../constants/colors";
+
+const StyledText = props => (
+  <Text {...props} style={[styles.text, props.style]} />
+);
+
+export default StyledText;
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.veryLightGray,
+    fontFamily: "space-mono"
+  }
+});
