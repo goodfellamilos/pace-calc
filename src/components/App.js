e93f05dda7c83b2a6be216bda7c9a3d0d4acb8ac
Milos Nikolic <mnikolic987@gmail.com>
2020-05-03 15:33:25 +0200

    Update App.js with SplashScreen.hide()

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,45 @@
 import "react-native-gesture-handler";
-
 import React from "react";
 import { Platform, StatusBar, StyleSheet, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "react-native-splash-screen";
 
 import BottomTabNavigator from "../components/navigation/BottomTabNavigator";
 import colors from "../constants/colors";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      {Platform.OS === "ios" && <StatusBar barStyle="light-content" />}
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            options={{
-              headerStyle: {
-                backgroundColor: colors.veryDarkGray
-              },
-              headerTintColor: colors.moderatePink,
-              headerTitleStyle: {
-                fontFamily: "SpaceMono-Bold",
-                fontSize: 18
-              }
-            }}
-            component={BottomTabNavigator}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="light-content" />}
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                options={{
+                  headerStyle: {
+                    backgroundColor: colors.veryDarkGray
+                  },
+                  headerTintColor: colors.moderatePink,
+                  headerTitleStyle: {
+                    fontFamily: "SpaceMono-Bold",
+                    fontSize: 18
+                  }
+                }}
+                component={BottomTabNavigator}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
